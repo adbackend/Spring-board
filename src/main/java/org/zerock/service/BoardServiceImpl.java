c8c563e3f59a57e3c13619604e82b8ade2123546
yujeong <yyyy2549@gmail.com>
2022-07-21 17:57:06 +0900

    페이징 처리

--- src/main/java/org/zerock/service/BoardServiceImpl.java
+++ src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 import org.zerock.mapper.BoardMapper;
 
 import lombok.Setter;
@@ -18,37 +19,53 @@ public class BoardServiceImpl implements BoardService{
 	@Setter(onMethod_ = @Autowired)
 	private BoardMapper mapper;
 
-
+	//글등록
 	@Override
 	public void register(BoardVO board) {
 		log.info("register........" + board);
 		mapper.insertSelectKey(board);
 	}
 
+	//글상세
 	@Override
 	public BoardVO get(Long bno) {
 		return mapper.read(bno);
 	}
 
+	//글수정
 	@Override
 	public boolean modify(BoardVO board) {
 		return mapper.update(board) == 1;
 	}
 
+	//글삭제
 	@Override
 	public boolean remove(Long bno) {
 
 		return mapper.delete(bno) == 1;
 	}
-
+	
+//	//목록
+//	@Override
+//	public List<BoardVO> getList() {
+//
+//		log.info("getList................");
+//
+//		return mapper.getList();
+//	}
+	
 	@Override
-	public List<BoardVO> getList() {
-
-		log.info("getList................");
-
-		return mapper.getList();
+	public List<BoardVO> getList(Criteria cri){
+		
+		log.info("get List with critera" + cri);
+		
+		return mapper.getListWithPaging(cri);
 	}
 
-
-
 }
+
+
+
+
+
+
